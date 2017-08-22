ba807561f6101aa5e06b34fca4f7c0ae84c9977a
Elman (Al) Krinker <elman.krinker@uspto.gov>
2017-08-22 16:05:57 -0400

    finished keyword extraction

--- keyphrase_extraction_util.py
+++ keyphrase_extraction_util.py
@@ -3,12 +3,19 @@
 # text document(s) are captured in these key phrases.
 from operator import itemgetter
 import nltk
-# Bigrams
+import itertools
+from text_normalization_util import normalize_document
+# Bigrams collocations
 from nltk.collocations import BigramCollocationFinder
 from nltk.collocations import BigramAssocMeasures
-# Trigrams
+# Trigrams collocations
 from nltk.collocations import TrigramCollocationFinder
 from nltk.collocations import TrigramAssocMeasures
+# Weighted Tag-Based Phrase Extraction
+from gensim import corpora, models
+
+
+stopword_list = nltk.corpus.stopwords.words('english')
 
 
 def flatten_corpus(corpus):
@@ -57,3 +64,48 @@ def retrieve_top_trigrams_collocations(corpus, top=5, measure='pmi'):
         raise ValueError('Type of measure is unknown!')
 
     return top_trigrams
+
+
+def extract_grammar_phrases(corpus, phrase_grammar_pattern):
+    # build phrase list based on grammar pattern
+    all_phrases = []
+    grammar_pattern = nltk.chunk.regexp.RegexpParser(phrase_grammar_pattern)
+
+    sentences = normalize_document(corpus, esc_html=False, expand_cont=False, lemmatize=False, tokenize=False,
+                                   remove_special_char=False, remove_stop_words=False)
+    for sentence in sentences:
+        # POS tag sentences
+        tagged_sentences = nltk.pos_tag_sents([nltk.word_tokenize(sentence)])
+
+        # extract phrases based on pattern
+        phrases = [grammar_pattern.parse(tagged_sentence) for tagged_sentence in tagged_sentences]
+
+        # extract word, pos tag, tag triples
+        wtc_sentences = [nltk.chunk.tree2conlltags(phrase) for phrase in phrases]
+        flattened_phrases = list(itertools.chain.from_iterable(wtc_sentence for wtc_sentence in wtc_sentences))
+
+        # get valid phrase based on tags
+        valid_phrases_tagged = [(status, [wtc for wtc in chunk])
+                                for status, chunk
+                                in itertools.groupby(flattened_phrases, lambda (word, pos, chunk): chunk != 'O')]
+
+        valid_phrases = [' '.join(word.lower() for word, tag, chunk in wtc_group if word.lower() not in stopword_list)
+                                 for status, wtc_group in valid_phrases_tagged if status]
+
+        all_phrases.append(valid_phrases)
+
+    return all_phrases
+
+
+def retrieve_weighted_tagbased_phrase(corpus, phrase_grammar_pattern=r'NP: {<DT>? <JJ>* <NN.*>+}', top=5):
+    phrases = extract_grammar_phrases(corpus, phrase_grammar_pattern)
+    # build tf-idf based model
+    dictionary = corpora.Dictionary(phrases)
+    corpus = [dictionary.doc2bow(phrase) for phrase in phrases]
+    tfidf = models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+    # get phrases and their tf-idf weights
+    weighted_phrases = {dictionary.get(id): round(value, 3) for doc in corpus_tfidf for id, value in doc}
+    weighted_phrases = sorted(weighted_phrases.items(), key=itemgetter(1), reverse=True)
+
+    return weighted_phrases[:top]
--- tests/test_text_summarization.py
+++ tests/test_text_summarization.py
@@ -4,45 +4,64 @@
 from nltk.corpus import gutenberg
 from text_normalization_util import normalize_document
 from keyphrase_extraction_util import retrieve_top_ngrams_collocations, retrieve_top_bigrams_collocations, \
-    retrieve_top_trigrams_collocations
+    retrieve_top_trigrams_collocations, retrieve_weighted_tagbased_phrase
 
 alice = gutenberg.sents(fileids='carroll-alice.txt')
 alice = [' '.join(ts) for ts in alice]
 
 norm_alice = filter(None, normalize_document(alice, lemmatize=False))
 
-print "\n$ Raw text"
-print alice[0]
-print alice[1]
-print alice[2]
-print alice[3]
+# print "\n$ Raw text"
+# print alice[0]
+# print alice[1]
+# print alice[2]
+# print alice[3]
+#
+# print "\n$ Normalized text"
+# print norm_alice[0]
+# print norm_alice[1]
+# print norm_alice[2]
+# print norm_alice[3]
+#
+# result = retrieve_top_ngrams_collocations(corpus=norm_alice, ngram_size=2, top=10)
+# print "\n$ Print top 10 bigrams (manually)"
+# print result
+#
+# # find raw frequencies of bigrams
+# result = retrieve_top_bigrams_collocations(corpus=norm_alice, top=10, measure='frequency')
+# print "\n$ Print top 10 bigrams (raw frequences)"
+# print result
+#
+# # find pointwise mutual information of bigrams
+# result = retrieve_top_bigrams_collocations(corpus=norm_alice, top=10, measure='pmi')
+# print "\n$ Print top 10 bigrams (pointwise mutal information)"
+# print result
+#
+# # find raw frequencies of trigrams
+# result = retrieve_top_trigrams_collocations(corpus=norm_alice, top=10, measure='frequency')
+# print "\n$ Print top 10 trigrams (raw frequences)"
+# print result
+#
+# # find pointwise mutual information of trigrams
+# result = retrieve_top_trigrams_collocations(corpus=norm_alice, top=10, measure='pmi')
+# print "\n$ Print top 10 trigrams (pointwise mutal information)"
+# print result
 
-print "\n$ Normalized text"
-print norm_alice[0]
-print norm_alice[1]
-print norm_alice[2]
-print norm_alice[3]
+toy_text = """
+Elephants are large mammals of the family Elephantidae
+and the order Proboscidea. Two species are traditionally recognised,
+the African elephant and the Asian elephant. Elephants are scattered
+throughout sub-Saharan Africa, South Asia, and Southeast Asia. Male
+African elephants are the largest extant terrestrial animals. All
+elephants have a long trunk used for many purposes,
+particularly breathing, lifting water and grasping objects. Their
+incisors grow into tusks, which can serve as weapons and as tools
+for moving objects and digging. Elephants' large ear flaps help
+to control their body temperature. Their pillar-like legs can
+carry their great weight. African elephants have larger ears
+and concave backs while Asian elephants have smaller ears
+and convex or level backs.
+"""
 
-result = retrieve_top_ngrams(corpus=norm_alice, ngram_size=2, top=10)
-print "\n$ Print top 10 bigrams (manually)"
-print result
-
-# find raw frequencies of bigrams
-result = retrieve_top_bigrams_collocations(corpus=norm_alice, top=10, measure='frequency')
-print "\n$ Print top 10 bigrams (raw frequences)"
-print result
-
-# find pointwise mutual information of bigrams
-result = retrieve_top_bigrams_collocations(corpus=norm_alice, top=10, measure='pmi')
-print "\n$ Print top 10 bigrams (pointwise mutal information)"
-print result
-
-# find raw frequencies of trigrams
-result = retrieve_top_trigrams_collocations(corpus=norm_alice, top=10, measure='frequency')
-print "\n$ Print top 10 trigrams (raw frequences)"
-print result
-
-# find pointwise mutual information of trigrams
-result = retrieve_top_trigrams_collocations(corpus=norm_alice, top=10, measure='pmi')
-print "\n$ Print top 10 trigrams (pointwise mutal information)"
+result = retrieve_weighted_tagbased_phrase(corpus=alice, top=50)
 print result
